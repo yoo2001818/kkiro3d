924eb74de23cb9ecc3bb7b4f5f66ae0fe03473bc
Duknam Yoo <yoo2001818@gmail.com>
2016-11-20 04:29:07 +0900

    Implement instancing. Resolves #30.

--- src/view/renderer/effect/meshInstanced.js
+++ src/view/renderer/effect/meshInstanced.js
@@ -3,52 +3,105 @@ export default function meshEffect(renderer) {
   const engine = renderer.engine;
   const webglue = renderer.webglue;
   const gl = renderer.webglue.gl;
-  let family = engine.systems.family.get('mesh', 'transform');
   let cache = {};
+  let entityCache = [];
+  function handleAdd(entity) {
+    if (!entity.mesh.visible) return;
+    let material = renderer.getSystem().materials[entity.mesh.material];
+    if (material == null) return;
+    let shader = renderer.getSystem().shaders[material.shader];
+    // Read the vertex shader - Does it support instancing?
+    if (shader.instancing == null) {
+      shader.instancing =
+        shader.source.vert.indexOf('#define INSTANCING') !== -1;
+    }
+    // Bail out if it doesn't support instancing
+    if (shader.instancing === false) return;
+    let key = entity.mesh.geometry + '_' + entity.mesh.material;
+    if (cache[key] == null) {
+      cache[key] = {
+        buffer: null,
+        geom: null,
+        list: [],
+        invalid: true,
+        invalidList: [],
+        geometry: renderer.getSystem().geometries[entity.mesh.geometry],
+        shader: renderer.getSystem().shaders[material.shader],
+        material: material
+      };
+    }
+    entityCache[entity.id] = key;
+    cache[key].list.push(entity);
+    // Do complete reupload
+    cache[key].invalid = true;
+  }
+  function handleRemove(entity) {
+    let keyEntry = entityCache[entity.id];
+    if (keyEntry == null) return;
+    let entry = cache[keyEntry];
+    if (entry == null) return;
+    let offset = entry.list.indexOf(entity);
+    if (offset !== -1) {
+      entry.list.splice(offset, 1);
+    }
+    entry.invalid = true;
+    if (entry.list.length === 0) {
+      // Dispose
+      entry.buffer.dispose();
+      entry.geom.dispose();
+      delete cache[keyEntry];
+    }
+    entityCache[entity.id] = null;
+  }
+  function propagateUpdate(id) {
+    let childrens = engine.systems.parent.childrens[id];
+    let key = entityCache[id];
+    let entry = cache[key];
+    if (entry != null) {
+      if (entry.invalidList.indexOf(id) === -1) {
+        entry.invalidList.push(id);
+      }
+    }
+    if (childrens == null) return;
+    childrens.forEach(e => propagateUpdate(e));
+  }
+  let family = engine.systems.family.get('mesh', 'transform');
+  family.onAdd.add(handleAdd);
+  family.onRemove.add(handleRemove);
+  engine.attachHook('external.load!', () => {
+    // Remove ALL data (Start everything again)
+    for (let key in cache) {
+      let entry = cache[key];
+      if (entry.geom != null) {
+        entry.geom.dispose();
+        entry.buffer.dispose();
+      }
+    }
+    cache = {};
+  });
+  engine.attachHook('mesh.*:post!', ([entity]) => {
+    handleRemove(entity);
+    if (entity.mesh.visible) handleAdd(entity);
+  });
+  engine.attachHook('transform.*!', ([entity]) => {
+    propagateUpdate(entity.id);
+  });
+  engine.attachHook('parent.*!', ([entity]) => {
+    propagateUpdate(entity.id);
+  });
   return {
     worldPre: (world) => {
-      // TODO We have make it to external system or something
-      // (This is only proof of concept, which is horribly slow)
-      // Clear cache list
-      for (let key in cache) {
-        cache[key].list = [];
-      }
-      // Iterate all the entities and pre-build cache
-      family.entities.forEach(entity => {
-        if (!entity.mesh.visible) return;
-        let material = renderer.getSystem().materials[entity.mesh.material];
-        if (material == null) return;
-        let shader = renderer.getSystem().shaders[material.shader];
-        // Read the vertex shader - Does it support instancing?
-        if (shader.instancing == null) {
-          shader.instancing =
-            shader.source.vert.indexOf('#define INSTANCING') !== -1;
-        }
-        // Bail out if it doesn't support instancing
-        if (shader.instancing === false) return;
-        // TODO Possibly cache this too
-        let key = entity.mesh.geometry + '_' + entity.mesh.material;
-        // Put everything to the cache
-        if (cache[key] == null) {
-          cache[key] = {
-            buffer: null,
-            geom: null,
-            list: [],
-            geometry: renderer.getSystem().geometries[entity.mesh.geometry],
-            shader: renderer.getSystem().shaders[material.shader],
-            material: material
-          };
-        }
-        cache[key].list.push(engine.systems.matrix.get(entity));
-      });
       // Update / create VBO
       for (let key in cache) {
         let entry = cache[key];
-        if (entry.list.length > 2) {
+        if (entry.invalid) {
+          if (entry.list.length < 2 && entry.geom == null) continue;
+          entry.invalidList = [];
           // Unwrap the list
           let unwrapped = new Float32Array(entry.list.length * 16);
           for (let i = 0; i < entry.list.length; ++i) {
-            unwrapped.set(entry.list[i], i * 16);
+            let entity = entry.list[i];
+            unwrapped.set(engine.systems.matrix.get(entity), i * 16);
           }
           if (entry.geom == null) {
             // Create VBO
@@ -102,14 +155,16 @@ export default function meshEffect(renderer) {
               }
             });
           }
-        } else {
-          if (entry.geom != null) {
-            // Dispose VBO and geometry
-            entry.buffer.dispose();
-            entry.buffer = null;
-            entry.geom.dispose();
-            entry.geom = null;
-          }
+          entry.invalid = false;
+          continue;
+        }
+        while (entry.invalidList.length > 0) {
+          let entityId = entry.invalidList.pop();
+          let entity = engine.state.entities[entityId];
+          if (entity == null) continue;
+          let mat = engine.systems.matrix.get(entity);
+          let index = entry.list.indexOf(entity);
+          entry.buffer.updateSub(mat, index * 16);
         }
       }
       return world;
@@ -132,9 +187,7 @@ export default function meshEffect(renderer) {
       if (data === false) return data;
       if (entity.transform == null) return data;
       if (entity.mesh == null || !entity.mesh.visible) return data;
-      let material = renderer.getSystem().materials[entity.mesh.material];
-      if (material == null) return data;
-      let entry = cache[entity.mesh.geometry + '_' + entity.mesh.material];
+      let entry = cache[entityCache[entity.id]];
       if (entry == null || entry.buffer == null) return null;
       return false;
     }
